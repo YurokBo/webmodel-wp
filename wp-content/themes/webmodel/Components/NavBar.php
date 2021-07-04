9fd4ccb2203ab8781c7e6c672cf86b6cb27c0ca9
YurokBo <vertel3012@mail.ru>
2021-07-04 14:15:20 +0300

    main added nav, header, fonts, pages, styles

---
+++ wp-content/themes/webmodel/Components/NavBar.php
@@ -0,0 +1,23 @@
+<nav class="Nav">
+    <div class="Container">
+        <div class="Nav-Content">
+            <a href="/" class="Nav-Logo">
+                <img src="./images/logo.png" alt="logo">
+            </a>
+            <ul class="Nav-List">
+                <li class="Nav-Item">
+                    <a href="../questions.php" class="Nav-Link">вопросы</a>
+                </li>
+                <li class="Nav-Item">
+                    <a href="../registration.php" class="Nav-Link">регистрация</a>
+                </li>
+                <li class="Nav-Item">
+                    <a href="../contacts.php" class="Nav-Link">контакты</a>
+                </li>
+            </ul>
+        </div>
+        <!--<div class="nav__burger">
+            <span></span>
+        </div>-->
+    </div>
+</nav>
--- wp-content/themes/webmodel/index.php
+++ wp-content/themes/webmodel/index.php
@@ -5,9 +5,39 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/main.css">
     <title>Вебмодель на дому</title>
 </head>
 <body>
+
+<?php
+include('./Components/NavBar.php')
+?>
+
+<section class="Header">
+    <div class="Container">
+        <div class="Header-Inner">
+            <div class="Header-Content">
+                <h1 class="Title Title--h1 Header-Title">вебмодель на дому</h1>
+                <ul class="Header-List">
+                    <li class="Header-Item">свободный график</li>
+                    <li class="Header-Item">ежедневные выплаты</li>
+                    <li class="Header-Item">персональное обучение</li>
+                </ul>
+                <a href="/registration.php" class="Btn Btn--pink Header-Btn">
+                    Заполнить анкету
+                </a>
+            </div>
+            <div class="Header-ImageBox">
+                <img src="images/header-img.png" alt="girls" class="Header-Image">
+            </div>
+        </div>
+        <div class="Decor Header-Decor">
+            Будь смелее!
+        </div>
+    </div>
+</section>
+
 
 </body>
 </html>
