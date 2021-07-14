c07a7b886d1ca21b69f35d9444e05eeec47b1332
YurokBo <vertel3012@mail.ru>
2021-07-14 22:05:04 +0300

    main contacts  - added contacts

--- wp-content/themes/webmodel/contacts.php
+++ wp-content/themes/webmodel/contacts.php
@@ -7,6 +7,18 @@ include('./Components/Head.php')
             <h2 class="Title Title--h2 Contacts-Title">
                 Контакты
             </h2>
+            <div class="Contacts-Content">
+                <div class="Contacts-Contacts">
+                    <div class="Contacts-ContactsTitle">
+                        Контактная <br> информация
+                    </div>
+                    <div class="Contacts-ContactsContact">
+                        <a href="tel:+79118350110 " class="Contact Contacts-ContactsPhone">+7 (911) 835-01-10 </a>
+                        <div class="Contact Contacts-ContactsAddress">м. Маяковская, Невский пр.</div>
+                    </div>
+                </div>
+                <div class="Contacts-Map"></div>
+            </div>
         </div>
     </section>
 
