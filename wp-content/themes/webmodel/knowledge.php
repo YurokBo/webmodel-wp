5ee0857fdd81ee79a440ad27e06dd5f912ba676b
YurokBo <vertel3012@mail.ru>
2021-08-22 20:41:15 +0300

    main knowledge base page

--- wp-content/themes/webmodel/knowledge.php
+++ wp-content/themes/webmodel/knowledge.php
@@ -1,1 +1,73 @@
 <?php
+include('./Components/Head.php')
+?>
+
+<section class="Knowledge">
+    <div class="Container">
+        <h2 class="Title Title--h2 Knowledge-Title">
+            База знаний
+        </h2>
+        <div class="Knowledge-List">
+            <a href="" class="Knowledge-Item">
+                <div class="Knowledge-ItemLabel">
+                    статьи
+                </div>
+                <img src="images/temporary-photo.png" alt="photo" class="Knowledge-ItemPhoto">
+                <p class="Text Knowledge-ItemText">
+                    Секреты популярности среди англоязычных пользователей
+                </p>
+            </a>
+            <a href="" class="Knowledge-Item">
+                <div class="Knowledge-ItemLabel">
+                    практические советы
+                </div>
+                <img src="images/temporary-photo.png" alt="photo" class="Knowledge-ItemPhoto">
+                <p class="Text Knowledge-ItemText">
+                    Причины популярности вебкам сайта
+                </p>
+            </a>
+            <a href="" class="Knowledge-Item">
+                <div class="Knowledge-ItemLabel">
+                    практические советы
+                </div>
+                <img src="images/temporary-photo.png" alt="photo" class="Knowledge-ItemPhoto">
+                <p class="Text Knowledge-ItemText">
+                    10 профессий для дополнительной работы в кризис: советы для девушек
+                </p>
+            </a>
+            <a href="" class="Knowledge-Item">
+                <div class="Knowledge-ItemLabel">
+                    статьи
+                </div>
+                <img src="images/temporary-photo.png" alt="photo" class="Knowledge-ItemPhoto">
+                <p class="Text Knowledge-ItemText">
+                    Виды подработки
+                    во всемирной паутине.
+                </p>
+            </a>
+            <a href="" class="Knowledge-Item">
+                <div class="Knowledge-ItemLabel">
+                    практические советы
+                </div>
+                <img src="images/temporary-photo.png" alt="photo" class="Knowledge-ItemPhoto">
+                <p class="Text Knowledge-ItemText">
+                    Как найти высокооплачиваемую работу: советы для девушек
+                </p>
+            </a>
+            <a href="" class="Knowledge-Item">
+                <div class="Knowledge-ItemLabel">
+                    статьи
+                </div>
+                <img src="images/temporary-photo.png" alt="photo" class="Knowledge-ItemPhoto">
+                <p class="Text Knowledge-ItemText">
+                    Реально ли найти подработку на выходные?
+                </p>
+            </a>
+        </div>
+    </div>
+</section>
+
+
+<?php
+include('./Components/Footer.php')
+?>
